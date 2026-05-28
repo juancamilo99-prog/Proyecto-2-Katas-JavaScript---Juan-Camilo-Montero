49bfaad92310cf5a644bf4fb481bb703d537de5e
monterocamilo99@hotmail.com <monterocamilo99@hotmail.com>
2026-05-28 23:42:27 +0200

    katas javascript exercise 31

---
+++ ejercicio-basicos-avanzados/ejercicio31/ejercicio-31.js
@@ -0,0 +1,26 @@
+/* Dada una lista de artistas con sus influencias, 
+utiliza bucles anidados para 
+listar todos los artistas y 
+sus influencias directas e imprime cada par por consola. */
+
+const artists = [
+  { name: 'Kurt Cobain', influences: ['The Beatles', 'Pixies', 'Lead Belly'] },
+  { name: 'David Bowie', influences: ['Little Richard', 'Chuck Berry', 'The Velvet Underground'] },
+  { name: 'Eddie Vedder', influences: ['The Who', 'Neil Young', 'Jim Morrison'] },
+  { name: 'Freddie Mercury', influences: ['Liza Minnelli', 'Jimi Hendrix', 'Aretha Franklin'] },
+  { name: 'John Lennon', influences: ['Elvis Presley', 'Chuck Berry', 'Buddy Holly'] }
+];
+
+// Añade tu código de bucle aquí
+
+const artistaLength = [];
+
+for (let i = 0; i < artists.length; i++) {
+    const artista = artists[i];
+    //console.log(artista);
+    for (let j = 0; j < artista.influences.length; j++) {
+       console.log(artista.name + " -> "+ artista.influences[j]);
+    }
+}
+
+
